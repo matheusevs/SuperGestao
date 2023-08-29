cf6e3197a8d9b36b8b9bf5310f6fad3903a25519
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-29 08:56:37 -0300

    40. Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,13 @@ Route::prefix('/app')->group(function(){
     Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
+
+Route::get('/rota1', function(){
+    echo 'rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1');
