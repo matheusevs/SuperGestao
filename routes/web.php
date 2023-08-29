ecd9582e6124b1ec7a62504a299d94ed09020274
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-29 08:49:50 -0300

    39. Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 //     return 'Home';
 // });
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function(){return 'login';});
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'clientes';});
-    Route::get('/fornecedores', function(){return 'fornecedores';});
-    Route::get('/produtos', function(){return 'produtos';});
+    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
