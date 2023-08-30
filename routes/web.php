44100553923b9b9c98383cfde5dbd4ef6032c14c
Matheus Eduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-30 19:40:46 -0300

    68. Enviando o formulário de contato via POST

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
