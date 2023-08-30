5b622f9ae0f9867d3bdbc82dec4a639432ce117c
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-08-30 14:48:08 -0300

    66. Blade - Realizando include de views (@include)

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -6,22 +6,7 @@
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     </head>    
     <body>
-        <div class="topo">
-
-            <div class="logo">
-                <img src="{{ asset('img/logo.png') }}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
-
+        @include('site.layouts._partials.topo')
         @yield('conteudo')
-        
     </body>
 </html>
