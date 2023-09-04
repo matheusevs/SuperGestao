a8fbbe3071a1dddde7a7a6550c944353adf2622b
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-09-04 16:32:30 -0300

    124. Validação de campos obrigatórios (required)

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,20 +8,23 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     
-    public function contato(Request $request){
-
-        // $contato = new SiteContato();
-        // $contato->nome = $request->input('nome');
-        // $contato->telefone = $request->input('telefone');
-        // $contato->email = $request->input('email');
-        // $contato->motivo_contato = $request->input('motivo_contato');
-        // $contato->mensagem = $request->input('mensagem');
-        // $contato->save();
-
-        $contato = new SiteContato();
-        $contato->create($request->all());
+    public function contato(){
 
         return view('site.contato');
+
+    }
+
+    public function salvar(Request $request){
+
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+
+        SiteContato::create($request->all());
 
     }
 
