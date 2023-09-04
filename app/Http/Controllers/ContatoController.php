c24b2b3ed2a975b4750e71bb4437efe65a2773b8
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-09-04 15:54:30 -0300

    123. Gravando os dados do formulário no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
@@ -9,9 +10,17 @@ class ContatoController extends Controller
     
     public function contato(Request $request){
 
-        echo '<pre>';
-            print_r($request->all());
-        echo '</pre>';
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // $contato->save();
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+
         return view('site.contato');
 
     }
