4cb3dc80022244bfafccc79cfbaebdfff0fa97d9
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-09-04 13:45:41 -0300

    117. Eloquent - Deletando registros com SoftDelete

--- database/migrations/2023_09_01_114742_create_fornecedores_table.php
+++ database/migrations/2023_09_01_114742_create_fornecedores_table.php
@@ -16,7 +16,11 @@ class CreateFornecedoresTable extends Migration
         Schema::create('fornecedores', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 50);
+            $table->string('site', 150)->nullable();
+            $table->string('uf', 2);
+            $table->string('email', 150);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
