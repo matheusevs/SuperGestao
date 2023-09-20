d12f5b10ef846f9e7f5f0c2da2090d2388dcbaa8
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-09-20 11:06:44 -0300

    131. Validação de campos e-mail

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -22,7 +22,7 @@ class ContatoController extends Controller
         $request->validate([
             'nome' => 'required|min:3|max:50',
             'telefone' => 'required',
-            'email' => 'required',
+            'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
