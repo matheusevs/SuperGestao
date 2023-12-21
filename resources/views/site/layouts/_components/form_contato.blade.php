f496fa8c18edf93fa1d96b4e0c621fc2b4f92fdc
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-12-21 09:26:18 -0300

    135. Customizando a apresentação dos erros de validação parte 2

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -3,10 +3,18 @@
 <form action="{{ route('site.contato') }}" method="post">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if ($errors->has('nome'))
+        {{ $errors->first('nome') }}
+    @endif
+
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
+
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
+
     <br>
     <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
@@ -15,18 +23,12 @@
             <option value="{{ $motivo->id }}" {{ old('motivo_contatos_id') == $motivo->id ? 'selected' : '' }}>{{ $motivo->motivo_contato }}</option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
+
     <br>
     <textarea name="mensagem" class="{{ $classe }}" placeholder="Preencha aqui a sua mensagem">{{ (!empty(old('mensagem'))) ? old('mensagem') : '' }}</textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
+
     <br>
     <button type="submit" value="{{ old('submit') }}" class="{{ $classe }}">ENVIAR</button>
 </form>
-
-@if ($errors->any())
-
-    <div>
-        @foreach ($errors->all() as $error)
-            {{ $error }}
-        @endforeach
-    </div>
-    
-@endif
