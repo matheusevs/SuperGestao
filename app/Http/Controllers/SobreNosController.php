c2f3c3c8c3c94f7e621db719d2b7a6728dd426a6
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-12-26 11:13:35 -0300

    140. Implementando middlewares no método construtor dos controllers

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(LogAcessoMiddleware::class);
+    }
     
     public function sobreNos(){
 
