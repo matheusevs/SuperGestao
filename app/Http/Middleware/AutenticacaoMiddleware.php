912e5bf56b3e7b2f865f848d3baba1c0fd109dbd
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-12-26 11:51:02 -0300

    145. Passando parâmetros para o middleware

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,8 +13,23 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
+        
+
+        if($metodo_autenticacao == 'padrao'){
+            echo "Verificar o usuário e senha no banco de dados. $perfil";
+        }
+
+        if($metodo_autenticacao == 'ldap'){
+            echo "Verificar o usuário e senha no AD. $perfil";
+        }
+
+        if($perfil == 'visitante'){
+            echo "Exibir apenas alguns recursos.";
+        } else {
+            echo "Carregar o perfil do banco de dados.";
+        }
 
         if(true){
             return $next($request);
