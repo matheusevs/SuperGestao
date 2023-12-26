587a201754902c4e46ca28e04665814dd9b56157
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-12-26 11:09:42 -0300

    139. Criando o model LogAcesso e sua migration

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\LogAcesso;
 use Closure;
 use Facade\FlareClient\Http\Response;
 
@@ -16,6 +17,10 @@ class LogAcessoMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $ip = $request->server->get('REMOTE_ADDR');
+        $route = $request->getRequestUri();
+
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $route"]);
         return $next($request);
     }
 }
