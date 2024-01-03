93ed288c5a63f490ba418976f809a6b99f4cf97b
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2024-01-03 16:53:34 -0300

    147. Implementando o formulário de Login

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
-Route::get('/login', function(){return 'login';})->name('site.login');
+Route::get('/login', 'LoginController@index')->name('site.login');
+Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function(){
 
