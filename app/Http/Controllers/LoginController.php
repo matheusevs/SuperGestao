88add59d434731297f47804d2c186a880ae1095e
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2024-01-03 23:36:27 -0300

    153. Adicionando a função logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -62,6 +62,7 @@ class LoginController extends Controller
     }
 
     public function sair(){
-        echo 'Sair';
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
